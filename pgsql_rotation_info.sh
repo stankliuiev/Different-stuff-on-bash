f2e7922fd5b375423bb2f99615cd4d2d8132c7c0
Stan <demonoid.wolf@gmail.com>
2022-10-26 14:28:56 +0200

    Update pgsql_rotation_info.sh

--- pgsql_rotation_info.sh
+++ pgsql_rotation_info.sh
@@ -2,6 +2,8 @@
 #set -x
 # this script is collecting time/stat iformation about last backup made by backup_script and paste it to /tmp/backup.txt, which can be read by nrpe, which deside
 # to give an alert or not based on age of backup in seconds.
+# it should run every 15 minutes:
+# */15 * * * * postgres     /usr/local/bin/pgsql_rotation_info.sh &>> /var/lib/pgsql/backups.log
   
 
 latest_backup_date=$(ls -td -- /var/lib/pgsql/12/backups/* | head -n 1);
